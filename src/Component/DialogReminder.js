a1c981304b55576c32631c3567665d4f3b04e42c
manojmansukh <manojmansukh7@gmail.com>
2020-01-25 11:22:12 +0530

    APK file create

---
+++ src/Component/DialogReminder.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import Dialog from "react-native-dialog";
+
+export default class DialogReminderr extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dialogVisible: false,
+      date: "Select The Date",
+      time: "",
+      setTime: "Select The Time",
+      isDatePickerVisible: false,
+      isTimePickerVisible: false,
+    };
+  }
+
+  convert12hour = (dm) => {
+    console.log('mkkkk', dm);
+
+    var timeString = dm;
+    var H = +timeString.substr(0, 2);
+    var h = H % 12 || 12;
+    var ampm = (H < 12 || H === 24) ? "AM" : "PM";
+    timeString = h + timeString.substr(2, 3) + ampm;
+    console.log("current time:", timeString);
+    this.setState({ setTime: timeString })
+  }
+  _showDatePicker = () => this.setState({ isDatePickerVisible: true });
+
+  _hideDatePicker = () => this.setState({ isDatePickerVisible: false });
+
+  _showTimePicker = () => this.setState({ isTimePickerVisible: true });
+
+  _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
+
+  _handleDatePicked = (date) => {
+
+    var mj = JSON.stringify(date)
+    var Date1 = mj.slice(1, 11)
+    console.log(Date1);
+    this.setState({ date: Date1 })
+    // this._hideDatePicker();
+  };
+
+  _handleTimePicked = (date) => {
+    console.log('in time picker');
+    var d = " " + date
+    var dm = d.slice(17, 25)
+    this.convert12hour(dm);
+
+
+    var mj = JSON.stringify(date)
+    var Time1 = mj.slice(11, 25)
+    console.log(Time1);
+
+    this.setState({ time: Time1 })
+
+    //this._hideTimePicker();
+  };
+
+  handleNotification = () => {
+    console.log("concate");
+
+    var date = this.state.date + this.state.time
+    console.log(date);
+    var date23 = new Date(date)
+    
+    if (this.state.time !== null && this.state.date !== null) {
+      console.log("in if else");
+      this.props.handleSave(this.state.date, this.state.setTime, date23)
+    }
+    else {
+      alert('entrer all details')
+    }
+  }
+  render() {
+    return (
+
+      <Dialog.Container visible={this.props.dialogVisible}>
+        <Dialog.Title>Add Reminder</Dialog.Title>
+
+        <TouchableOpacity onPress={this._showDatePicker}>
+          <View style={{ margin: 4, flexDirection: 'row', alignSelf: 'center', backgroundColor: 'transparent', width: 200, borderWidth: 1, height: 40 }}>
+            <Image style={{ height: 25, width: 30, margin: 5 }}
+              source={require('../Image/Calendar.png')} />
+            <Text style={{ margin: 5, fontSize: 17 }}>{this.state.date}</Text>
+            <DateTimePicker
+              mode='date'
+              isVisible={this.state.isDatePickerVisible}
+              onConfirm={this._handleDatePicked}
+              onCancel={this._hideDatePicker}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this._showTimePicker}>
+          <View style={{ margin: 4, flexDirection: 'row', alignSelf: 'center', backgroundColor: 'transparent', width: 200, borderWidth: 1, height: 40 }}>
+            <Image style={{ height: 25, width: 30, margin: 5 }}
+              source={require('../Image/Alarm1.png')} />
+            <Text style={{ margin: 5, fontSize: 17 }}>{this.state.setTime}</Text>
+            <DateTimePicker
+              mode='time'
+              is24Hour={false}
+              isVisible={this.state.isTimePickerVisible}
+              onConfirm={this._handleTimePicked}
+              onCancel={this._hideTimePicker}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: 'row', top: 10, justifyContent: 'flex-end' }}>
+          <Dialog.Button label="Save"
+            onPress={() => this.handleNotification()
+              //this.props.handleSave(this.state.date,this.state.time)
+            }
+          />
+          <Dialog.Button label="Cancle" onPress={this.props.handleCancel} />
+        </View>
+
+      </Dialog.Container>
+
+    );
+  }
+}
---
+++ src/Component/EditeNote.js
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import { Appbar, Provider, Menu } from 'react-native-paper';
+import { StyleSheet, Image, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Icon } from 'react-native';
+import { View } from 'native-base';
+import BottomBar from './BottomBar'
+import DialogReminder from './DialogReminder'
+import moment from 'moment';
+import PushNotification from "react-native-push-notification"
+import { editNote, setReminder, moveToTrash } from '../Services/FireBaseDb'
+// import { editNote, setReminder, moveToTrash } from '../Services/AxiosDb'
+
+
+export default class EditeNotes extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      note: '',
+      title: '',
+      date: '',
+      time: '',
+      dataSource: [],
+      pin: '',
+      archive: '',
+      visible: false,
+      trash: '',
+      currentTime: '',
+      bgColor: ''
+    }
+    this.handleSaveNote = this.handleSaveNote.bind(this);
+  }
+  showDialog = () => {
+    console.log('mj');
+    this.setState({ dialogVisible: true }, () => {
+      console.log('Hiii' + this.state.dialogVisible);
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({ dialogVisible: false }, () => {
+      console.log("handle cancle");
+    });
+  };
+
+  handleDelete = () => {
+    this.setState({ trash: true }, () => {
+      var currentNoteId = this.props.navigation.state.params.currentNoteId
+      //firebase method
+      moveToTrash(currentNoteId, this.state.trash)
+      this.props.navigation.navigate('Notes')
+    })
+  }
+
+  handleSaveReminder = (date, time, dateTime) => {
+    console.log(dateTime);
+
+    PushNotification.localNotificationSchedule({
+      //... You can use all the options from localNotifications
+      message: this.state.title, // (required)
+      subText: this.state.note,
+      date: dateTime // in 60 secs
+    });
+
+    this.setState({ date: date }, () => { console.log("Date:" + this.state.date); })
+    this.setState({ time: time }, () => {
+      var currentNoteId = this.props.navigation.state.params.currentNoteId
+      //firebase Method
+      setReminder(currentNoteId, this.state.date, this.state.time)
+
+
+    })
+    console.log(this.props.navigation.state.params.currentNoteId);
+    this.setState({ dialogVisible: false }, () => {
+    });
+  };
+
+  handleSaveNote = () => {
+
+    if (this.state.note == '' && this.state.title == '') {
+      this.props.navigation.navigate('Notes')
+    }
+    else {
+      var currentNoteId = this.props.navigation.state.params.currentNoteId
+      //firebase Method
+      editNote(currentNoteId, this.state.title, this.state.note, this.state.pin, this.state.archive, this.state.trash, this.state.bgColor)
+    }
+  }
+
+  handleNoteBgColour = (color) => this.setState({ bgColor: color });
+
+  componentDidMount() {
+    console.log('archive:', this.props.navigation.state.params.inform);
+    var date = moment()
+      .utcOffset('+05:30')
+      .format(' hh:mm A');
+
+    this.setState({
+      dataSource: this.props.navigation.state.params.inform,
+      currentTime: date
+    }, () => {
+      console.log("mjjj:", this.state.dataSource.color);
+      this.setState({
+        title: this.state.dataSource.Title,
+        note: this.state.dataSource.Note,
+        pin: this.state.dataSource.Pin,
+        archive: this.state.dataSource.Archive,
+        trash: this.state.dataSource.Trash,
+        bgColor: this.state.dataSource.BgColor
+      })
+
+    })
+  }
+  render() {
+    let selectedColor = '#fff';
+    console.log(this.props.navigation.state.params);
+    console.log("pin status:" + this.state.col);
+
+    console.log(this.props.navigation.state.params.currentNoteId);
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Appbar style={styles.top}>
+          <Appbar.Action icon={require('../Image/arrow_back.png')}
+            onPress={() => {
+              this.handleSaveNote()
+              this.props.navigation.navigate('Notes')
+            }
+            } />
+          <View style={{ flexDirection: 'row', flex: 1, alignSelf: 'flex-end', justifyContent: 'flex-end' }}>
+            <Appbar.Action icon={this.state.pin ? require('../Image/pin.png') : require('../Image/unpin.png')}
+              onPress={() => this.setState({ pin: !this.state.pin }, () => { console.log("manoj" + this.state.pin) })} />
+            <Appbar.Action icon={require('../Image/bell1.png')} onPress={this.showDialog} />
+            <Appbar.Action icon={this.state.archive ? require('../Image/Archive.png') : require('../Image/Unarchive.png')}
+              onPress={() => this.setState({ archive: !this.state.archive })} />
+            <Appbar.Action icon={require('../Image/Deleted.png')} onPress={() => this.handleDelete()} />
+          </View>
+
+          <DialogReminder dialogVisible={this.state.dialogVisible}
+            handleCancel={this.handleCancel}
+            handleSave={this.handleSaveReminder} />
+        </Appbar>
+
+        <View style={{ height: '84%', width: '100%', backgroundColor: this.state.bgColor }}>
+          <TextInput multiline={true}
+            style={styles.input}
+            value={this.state.title}
+            underlineColorAndroid="transparent"
+            placeholder="Title"
+            onChangeText={(text) => this.setState({ title: text })}>
+          </TextInput>
+
+          <TextInput multiline={true}
+            style={styles.input}
+            value={this.state.note}
+            underlineColorAndroid="transparent"
+            placeholder="Note"
+            onChangeText={(text) => this.setState({ note: text })}>
+          </TextInput>
+
+        </View>
+        <BottomBar handleBgColour={this.handleNoteBgColour} />
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  top: {
+    backgroundColor: 'white',
+    width: '100%',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderWidth: .1,
+  },
+  input: {
+    width: '100%',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderWidth: .1,
+    fontSize: 30,
+
+
+
+  },
+});
